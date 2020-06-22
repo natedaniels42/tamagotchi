c9d7751b70cda917e342e1e8b0c7d9c79198a5a7
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-06-22 18:51:58 -0500

    Touched up the css a bit and played around with the images

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ class Tamagotchi {
 	constructor(name) {
 		this.name = name;
 	}
-	feed() {
+	eat() {
 		if (document.getElementById('hb1').style.backgroundColor === 'white') {
 		death();
 	} else if (document.getElementById('hb5').style.backgroundColor === 'white') {
@@ -37,12 +37,13 @@ const feedLevel = $('#hb3, #hb4, #hb5, #hb6, #hb7, #hb8, #hb9, #hb10');
 const tiredLevel = $('#tb3, #tb4, #tb5, #tb6, #tb7, #tb8, #tb9, #tb10');
 const boredLevel = $('#bb3, #bb4, #bb5, #bb6, #bb7, #bb8, #bb9, #bb10');
 const yourTamagotchi = new Tamagotchi();
+
 function hatchFunc() {
 	yourTamagotchi.name = prompt('give your Tamagotchi a name');
 	$('#name').append(`<h1>Hi! My name is ${yourTamagotchi.name}`);
 	$('.button').css('visibility', 'visible');
 	$('#hatch-button').css('visibility', 'hidden');	
-	$('img').attr('src', 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/c0.0.768.768a/s552x414/57608888_10100620046004674_860073329942331392_o.jpg?_nc_cat=105&_nc_sid=da31f3&_nc_oc=AQmw7F0EqFOgAf5easEe6bB1YuyQtSBYXADjX3Rh8JIwZSz0xLbVp_JGOQr_YdVtePM&_nc_ht=scontent-dfw5-1.xx&oh=a7507529c8cbb1cb38c7b5018a89d450&oe=5F18A1C5');
+	$('img').attr('src', 'https://cdn1.thr.com/sites/default/files/imagecache/NFE_portrait/2011/11/waldo_pose_a_p.jpg');
 	startLevels.css('background-color', 'red');
 	return yourTamagotchi.name;
 }
@@ -54,5 +55,5 @@ function death() {
 
 
 $('#hatch-button').on('click', hatchFunc);
-$('#feed-button').on('click', yourTamagotchi.feed);
+$('#feed-button').on('click', yourTamagotchi.eat);
 $('#play-button').on('click', yourTamagotchi.play);
