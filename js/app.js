b4cc302f3b5d40f65c965a51ad8aa2d31689d34a
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-06-23 22:14:33 -0500

    Added a play again button and function

--- js/app.js
+++ js/app.js
@@ -5,19 +5,19 @@ class Tamagotchi {
 		this.age = 0;
 	}
 	eat() {
-		if (document.getElementById('hb1').style.backgroundColor === 'rgb(255, 255, 255)') {
-			death();
+		if (document.getElementById('hb2').style.backgroundColor === 'rgb(255, 255, 255)') {
+			$('#hb1').css('background-color', 'rgb(255, 255, 255)');
 		} else if (document.getElementById('hb5').style.backgroundColor === 'rgb(255, 255, 255)') {
-			$('.hunger-bar').css('background-color', 'rgb(255, 255, 255)');
+			hungerMid.css('background-color', 'rgb(255, 255, 255)');
 		} else {
 			feedLevel.css('background-color', 'rgb(255, 255, 255)');
 		}
 	}
 	play() {
-		if (document.getElementById('bb1').style.backgroundColor === 'rgb(255, 255, 255)') {
-			death();
+		if (document.getElementById('bb2').style.backgroundColor === 'rgb(255, 255, 255)') {
+			$('#bb1').css('background-color', 'rgb(255, 255, 255)');
 		} else if (document.getElementById('bb5').style.backgroundColor === 'rgb(255, 255, 255)') {
-			$('.boredom-bar').css('background-color', 'rgb(255, 255, 255)');
+			boredMid.css('background-color', 'rgb(255, 255, 255)');
 		} else {
 			boredLevel.css('background-color', 'rgb(255, 255, 255)');
 		}
@@ -25,19 +25,23 @@ class Tamagotchi {
 	getSleep() {
 		this.sleep = true;
 		$('img').attr('src', 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/54211691_10100608712457194_3096844443173519360_o.jpg?_nc_cat=105&_nc_sid=8bfeb9&_nc_oc=AQkGoWSofA1lPCo0juNUZAG5VNvM9Ty6KNvV1Uuqg_jwKuu0Xw6IdpDPRp9mBM8xh5k&_nc_ht=scontent-dfw5-1.xx&oh=17073fa6ce784fa3ebbf3f1e5378fb8a&oe=5F17A3F0');
-
-
 	}
 	wakeUp() {
 		this.sleep = false;
 		$('img').attr('src', 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/57608888_10100620046004674_860073329942331392_o.jpg?_nc_cat=105&_nc_sid=8bfeb9&_nc_oc=AQkTToCoTq8rCGMzljNfaFAEp1aXJTZGgELqyt8953XJjzwCgSGvCv2PCBgTxW9CAuI&_nc_ht=scontent-dfw5-1.xx&oh=ced60ac15631b932c3c3b4ec5f4874d5&oe=5F18A580');
 	}
+	evolve() {}
 }
 
 const hatchButton = document.getElementById('hatch-button');
 const hungerTop = document.getElementById('hb10');
 const tiredTop = document.getElementById('tb10');
 const boredTop = document.getElementById('bb10');
+const hungerBottom = document.getElementById('hb1');
+const boredBottom = document.getElementById('bb1');
+const tiredBottom = $('#tb1');
+const hungerMid = $('#hb2, #hb3, #hb4, #hb5, #hb6, #hb7, #hb8, #hb9, #hb10');
+const boredMid = $('#bb2, #bb3, #bb4, #bb5, #bb6, #bb7, #bb8, #bb9, #bb10')
 const startLevels = $('#bb1, #bb2, #bb3, #bb4, #bb5, #hb1, #hb2, #hb3, #hb4, #hb5, #tb1, #tb2, #tb3, #tb4, #tb5');
 const feedLevel = $('#hb3, #hb4, #hb5, #hb6, #hb7, #hb8, #hb9, #hb10');
 const tiredLevel = $('#tb3, #tb4, #tb5, #tb6, #tb7, #tb8, #tb9, #tb10');
@@ -48,7 +52,7 @@ let time = 0;
 function startTimer() {
 	const timer = setInterval(function() {
 		let tamagotchi = yourTamagotchi;
-		if (hungerTop.style.backgroundColor !== 'rgb(255, 0, 0)' && tiredTop.style.backgroundColor !== 'rgb(255, 0, 0)' && boredTop.style.backgroundColor !== 'rgb(255, 0, 0)') {
+		if (hungerTop.style.backgroundColor !== 'rgb(255, 0, 0)' && tiredTop.style.backgroundColor !== 'rgb(255, 0, 0)' && boredTop.style.backgroundColor !== 'rgb(255, 0, 0)' && hungerBottom.style.backgroundColor !== 'rgb(255, 255, 255)' && boredBottom.style.backgroundColor !== 'rgb(255, 255, 255)') {
 			time++;
 			console.log(time);
 			console.log(tamagotchi);
@@ -108,14 +112,14 @@ function boredTimer() {
 
 function pickName() {
 	yourTamagotchi.name = prompt('give your Tamagotchi a name');
-	$('#name-button').remove();
+	$('#name-button').css('visibility', 'hidden');
 	$('.name-paragraph').remove();
 	$('#hatch-button').css('visibility', 'visible');
 	return yourTamagotchi.name;
 }
 
 function hatch() {
-	$('#name').append(`<h1>Hi! My name is ${yourTamagotchi.name}`);
+	$('#name').append(`<h1 id="greeting">Hi! My name is ${yourTamagotchi.name}`);
 	$('.button').css('visibility', 'visible');
 	$('#hatch-button').css('visibility', 'hidden');	
 	$('.buttons h1').remove();
@@ -128,7 +132,9 @@ function hatch() {
 function death() {
 	$('img').attr('src', 'https://image.shutterstock.com/image-vector/gravestone-on-white-background-vector-260nw-199099982.jpg');
 	$('.hatch').prepend(`<h1>${yourTamagotchi.name} has died. you should think about whether you are responsible enough for a pet`);
-	$('.buttons').remove();
+	$('.buttons button').css('visibility', 'hidden');
+	$('#greeting').remove();
+	$('#play-again').css('visibility', 'visible');
 }
 
 function birthday() {
@@ -136,6 +142,14 @@ function birthday() {
 	$('.age span').text(`${yourTamagotchi.age}`);	
 }
 
+function playAgain() {
+	$('#name').append("<p class='name-paragraph'>Click here to pick a name");
+	$('#name-button').css('visibility', 'visible');
+	$('.hatch h1').remove();
+	$('#play-again').css('visibility', 'hidden');
+	$('.age span').remove();
+	$('div').css('background-color', 'rgb(255, 255, 255)');
+}
 
 $('#name-button').on('click', pickName);
 $('#hatch-button').on('click', hatch);
@@ -147,7 +161,7 @@ $('#wake-up-button').on('click', function() {
 })
 $('#feed-button').on('click', yourTamagotchi.eat);
 $('#play-button').on('click', yourTamagotchi.play);
-
+$('#play-again').on('click', playAgain);
 
 
 
